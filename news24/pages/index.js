1c4d753f0cb217f946e995d53f8e02cdbbd37334
Giriraj Roy <girirajroy2001@gmail.com>
2022-03-21 17:04:19 +0530

    Home page updated

--- news24/pages/index.js
+++ news24/pages/index.js
@@ -2,7 +2,14 @@ import Link from 'next/link'
 
 function Home() {
   return <>
-    <h1>News app</h1>
+    <div className='Heading'>
+      <div className='navbar'>
+        <div>Home</div>
+        <div>Careers</div>
+        <div>About</div>
+        <div>Contact</div>
+      </div>
+      <h1>Saber Daily</h1></div>
     <br/>
     <Link href="/news">
       <a> <button>Latest News </button></a>
